a8128bf84f52fdba29bf69757034df0dc7708536
Avaltrois <avaltrois@gmail.com>
2019-03-15 20:31:24 +0200

    JUnit Tests

---
+++ chapter_001/src/test/java/ua/learningjava/calculate/CalculateTest.java
@@ -0,0 +1,23 @@
+package ua.learningjava.calculate;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+* Test.
+*
+* @author Avaltrois
+* @version $Id$
+* @since 0.1
+*/
+
+public class CalculateTest {
+@Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Avaltrois";
+    String expect = "Echo, echo, echo : Avaltrois"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+}
+
---
+++ chapter_001/src/test/java/ua/learningjava/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Test task.
+*
+* @author Avaltrois
+* @version $Id$
+* @since 0.1
+*/
+package ua.learningjava.calculate;
