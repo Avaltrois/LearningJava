84f3edc260deee4287a63890ef0dc0ea977feeed
Avaltrois <avaltrois@gmail.com>
2019-03-15 20:00:52 +0200

    JavaDoc

--- chapter_001/src/main/java/ua/learningjava/calculate/Calculate.java
+++ chapter_001/src/main/java/ua/learningjava/calculate/Calculate.java
@@ -1,6 +1,12 @@
 package ua.learningjava.calculate;
-
+/**
+* Calculate.
+* @author Avaltrois
+*/
 public class Calculate {
+	/**
+	* Main.
+	* @param args - args.
 	public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ chapter_001/src/main/java/ua/learningjava/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Avaltrois
+* @version $Id$
+* @since 0.1
+*/
+package ua.learningjava.calculate;
