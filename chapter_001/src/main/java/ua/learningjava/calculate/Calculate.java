891fc7b001a7a4c127a077b54e772a4d206a0991
Avaltrois <avaltrois@gmail.com>
2019-03-25 18:21:14 +0200

    Inspect Code

--- chapter_001/src/main/java/ua/learningjava/calculate/Calculate.java
+++ chapter_001/src/main/java/ua/learningjava/calculate/Calculate.java
@@ -1,23 +1,23 @@
 package ua.learningjava.calculate;
 /**
-* Calculate.
-* @author Avaltrois
-*/
+ * Calculate.
+ * @author Avaltrois
+ */
 public class Calculate {
 	/**
-	* Main.
-	* @param args - args.
-	*/
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+		System.out.println("Hello World!");
+	}
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
-}
+	}
 
 }
--- chapter_001/src/test/java/ua/learningjava/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ua/learningjava/calculate/CalculateTest.java
@@ -3,21 +3,21 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
-* Test.
-*
-* @author Avaltrois
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Avaltrois
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class CalculateTest {
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Avaltrois";
-    String expect = "Echo, echo, echo : Avaltrois"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Avaltrois";
+        String expect = "Echo, echo, echo : Avaltrois";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
 
