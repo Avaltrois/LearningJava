0d48c7b38c20ec464b668124af6092ffab5882fa
Avaltrois <Avaltrois@gmail.com>
2019-04-02 23:24:02 +0300

    Maximum of 2 numbers(Ternary Operator)

---
+++ chapter_001/src/main/java/ua/learningjava/max/Max.java
@@ -0,0 +1,17 @@
+package ua.learningjava.max;
+/**
+ * @author Avaltrois
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Maximum of 2 numbers
+     * @return max
+     */
+    public int max(int first, int second){
+        return first>second ? first : second;
+    }
+
+
+}
---
+++ chapter_001/src/test/java/ua/learningjava/max/MaxTest.java
@@ -0,0 +1,14 @@
+package ua.learningjava.max;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 10);
+        assertThat(result, is(10));
+    }
+
+}
