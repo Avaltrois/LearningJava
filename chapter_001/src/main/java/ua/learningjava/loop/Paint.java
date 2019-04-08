014ce67b3491b6ef2ba65e0d4ceb2ff79cb4a4d7
Avaltrois <Avaltrois@gmail.com>
2019-04-08 14:25:22 +0300

    Code Refactoring (BiPredicate)

--- chapter_001/src/main/java/ua/learningjava/loop/Paint.java
+++ chapter_001/src/main/java/ua/learningjava/loop/Paint.java
@@ -1,5 +1,5 @@
 package ua.learningjava.loop;
-
+import java.util.function.BiPredicate;
 /**
  * @author Avaltrois
  * @version $Id$
@@ -7,51 +7,34 @@ package ua.learningjava.loop;
  */
 public class Paint {
     public String RightPartOfTriangle(int height) {
-        //Buffer for the result.
-        StringBuilder screen = new StringBuilder();
-        //width will be equal to height.
-        int width = height;
-        //the outer loop moves in rows.
-        for (int row = 0; row != height; row++) {
-            //the inner loop determines the position of the cell in the row.
-            for (int colomn = 0; colomn != width; colomn++) {
-                // if the string is equal to the cell, then draw ^.
-                // in this case, the line determines how much ^ will be on the line
-                if (row >= colomn) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // add line breaks.
-            screen.append(System.lineSeparator());
-        }
-        // Get the result.
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
 
     public String LeftPartOfTriangle(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int row = 0; row != height; row++) {
-            for (int colomn = 0; colomn != width; colomn++) {
-                if (row >= width - colomn - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
 
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+            for (int column = 0; column != weight; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -61,6 +44,4 @@ public class Paint {
         }
         return screen.toString();
     }
-
-
 }
